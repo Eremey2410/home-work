5f54a006e400a9b6b20b93ab5b1de4d81f643120
Eremey2410 <2410esv@gmail.com>
2022-05-16 23:29:05 +0300

    js

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,57 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+// let x = 5;
+// x = 10;
+// const age = 25;
+// const totalPrice = 200.74;
+// console.log('возраст:', age);
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+// const isOpen = true;
+// const type = typeof isOpen;
+// console.log(type);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
+// console.log('да');
+// alert('qweqwe');
+// console.log('нет');
+
+// const shouldRenew = confirm('хотите продлить');
+// console.log(shouldRenew);
+
+// let quantity = prompt('товары');
+// quantity = Number(quantity);
+// console.log(quantity);
+// console.log(typeof quantity);
+// const firstName = 'Chelsy';
+// const lastName = 'Emerald';
+// const type = 'VIP';
+// const room = 716;
+// const welcomeMsg = `Гость ${firstName} ${lastName} поселяется в ${type} номер ${room}`;
+// console.log(welcomeMsg);
+
+
+//
+// const topSpeed = 160;
+// const distance = 617.54;
+// const login = 'mango935';
+// const isOnline = true;
+// const isAdmin = false;
+// console.log(topSpeed);
+// console.log(distance);
+// console.log(login);
+// console.log(isOnline);
+// console.log(isAdmin);
+
+
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+
+// const totalPrice = pricePerItem * orderedQuantity;
+// console.log(totalPrice)
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+// console.log(message)
+function sayHi() {
+  console.log('Hello, this is my first function!');
+}
+sayHi();
